567aa3169d9e843765bef63648c2448137f41005
wbwriter <andreasfi@me.com>
2015-10-20 13:18:33 +0200

    Commentaire test gh

--- app/src/main/java/com/example/bugtracking/bugtracking/MainActivity.java
+++ app/src/main/java/com/example/bugtracking/bugtracking/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     //Mon comentaire test
+    // Commentaire test 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
