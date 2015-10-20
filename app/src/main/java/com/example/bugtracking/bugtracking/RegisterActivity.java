1ebd9704cef9afa968dae5b9a7ad0e044a04a2cb
wbwriter <andreasfi@me.com>
2015-10-20 16:07:54 +0200

    Register View and Controller Files

---
+++ app/src/main/java/com/example/bugtracking/bugtracking/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.bugtracking.bugtracking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
