22684141c703c619153863c80467b7372edba5ab
sylvaintauxe <sylvaintauxe@student.hevs.ch>
2015-10-20 21:42:01 +0200

    Création de "LoginActivity" avec le layout. Les boutons n'ont aucun effet (volontaire) car je n'ai pas créer les activity en relation avec ceux-ci.
    J'ai mis mes modification dans le fichier "String.xml" en commentaire, en espèrant que ça va éviter certains conflits pour le merge.
    
    Sylvain

---
+++ app/src/main/java/com/example/bugtracking/bugtracking/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.bugtracking.bugtracking;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public final  static String EXTRA_MESSAGE="com.example.bugtracking.bugtracking.MESSAGE";
+
+    public void clicLogin(View view){
+        /*Code provisoir pour naviguer entre les activity
+        il sera nécéssaire de faire un contrôle de mot de passe
+        avec la bae de donnée dans le futur
+         */
+
+        //Intent intent=new Intent(this, ProjectActivity.class);//Il faut créer la classe "Projectactivity pour que 4a fonctionne
+        EditText login=(EditText) findViewById(R.id.login);
+        String _login=login.getText().toString();
+
+       /* intent.putExtra(EXTRA_MESSAGE,_login);
+        startActivity(intent);*/
+    }
+
+    //Renvoie juste à l'Activity pour créer un nouveau Login
+    public void clicRegister(View view){
+      /*  Intent intent =new Intent(this, RegisterActivity);
+        startActivity(intent);*/
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
