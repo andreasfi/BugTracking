08a6026448334c085a75e2a3122003c791d54dba
sylvaintauxe <sylvaintauxe@student.hevs.ch>
2015-10-24 13:13:54 +0200

    Création du project activity avec le layout. Les items sont créés dans le fichier Java afin d'avoir un affichage dynamique. Tout s'affiche correctement, mais j'ai encore un souci pour ajouter une action sur les items. Cela n'affecte que le dernier item créé.
    Modification du Manifest pour l'affichage du menu.
    Ajout d'un layout "spécial" (activity_project_textview_layout) pour faire des tests.

---
+++ app/src/main/java/com/example/bugtracking/bugtracking/ProjectMainActivity.java
@@ -0,0 +1,130 @@
+package com.example.bugtracking.bugtracking;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class ProjectMainActivity extends AppCompatActivity {
+
+    TextView item;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       // setContentView(R.layout.activity_project_main);
+
+
+
+        /*Création d'un affichage dynamique fonctionne partiellement, probléme
+            avec le one clic listener. Ca fonctionne que sur le dernière item.
+         */
+        //TODO Essayer d'intergrer le layout "activity_project_textview_layout
+        LinearLayout ll=new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setPadding(16, 16, 16, 16);
+        //Création du boutton
+        Button addbutton=new Button(this);
+        addbutton.setText("Add");
+
+        //Création de la scrollbar
+        ScrollView scroll=new ScrollView(this);
+
+
+        //Création des Items
+        for(int i=0;i<15;i++){
+             item=new TextView(this);
+            item.setText("Item " + i);
+            item.setPadding(16, 16, 16, 16);
+
+            //Fonction utilisée lorsque l'on clic sur un élément
+           item.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   item.setText("New text");
+               }
+           });
+
+            ll.addView(item);
+
+        }
+         ll.addView(addbutton);
+        //Ici on ajoute le linearLayout dans le ScrollView
+        scroll.addView(ll);
+        setContentView(scroll);
+
+
+
+        //TODO Essyayer de récupérer les objet LinearLayout et TextView depuis le fichier xml
+        //tentative de récupérer les objets LinearLayout et TextView depuis le fichier xml. NE MARCHE PAS
+       /* LinearLayout ll2=(LinearLayout)findViewById(R.id.linearLayoutProject);
+        TextView item2=(TextView)findViewById(R.id.itemProject);
+        ll2.addView(item2);
+        setContentView(ll2);*/
+    }
+
+    //Methode "Listener" utilisée pas le fichier xml, non fonctionnel pour l'instant
+    public void goTo_Issue(View view){
+
+
+        TextView items[] =new TextView[5];
+        items[0]=(TextView)findViewById(R.id.itemProject);
+        items[1]=(TextView)findViewById(R.id.itemProject2);
+        items[2]=(TextView)findViewById(R.id.itemProject3);
+        items[3]=(TextView)findViewById(R.id.itemProject4);
+        items[4]=(TextView)findViewById(R.id.itemProject5);
+
+        if(items[0].isClickable()){
+            items[0].setText("Selected0");
+        }
+
+        if(items[1].isFocused()){
+            items[1].setText("Selected1");
+        }
+        if(items[3].isFocused()){
+            items[3].setText("Selected3");
+        }
+        if(items[4].isClickable()){
+            items[4].setText("Selected4");
+        }
+        if(items[2].isClickable()) {
+            items[2].setText("Selected2");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class onClicTextView {
+
+    }
+}
